100b0f3d5a536bc65ba5b445712d803677e7a287
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-09-18 00:51:17 +0300

    Update string_challenges.py

--- string_challenges.py
+++ string_challenges.py
@@ -6,17 +6,26 @@ print(word[-1:])
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-print(len(word))
+print(word.lower().count('а'))
 # ???
 
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
+
+count = 0
+
+for sign in word:
+    if sign in "аеёиоуыэюяАЕЁИОУЫЭЮЯ":
+        count += 1
+        
+print("Количество гласных букв:", count)
 # ???
 
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
+print(len(sentence.split(' ')))
 # ???
 
 
